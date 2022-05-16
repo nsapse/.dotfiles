a9e4eaa300e64ddfeae91139756ba66cd116ab9c
nsapse <nsapse@gmail.com>
2022-05-16 18:43:30 -0400

    added counsel bindings to emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,6 +2,9 @@
 ; disable the startup message
 (setq inhibit-startup-message t)
 
+; make the font readable
+
+
 (scroll-bar-mode -1) ;disable the visible scrollbars
 (tool-bar-mode -1)  ;disable the visible toolbar
 (tooltip-mode -1) ;disable tooltips
@@ -20,6 +23,14 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+; get additional package sources
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+
+
+
 (column-number-mode)
 (global-display-line-numbers-mode t)
 
@@ -28,6 +39,20 @@
 				shell-mode-hook
 				eshell-mode-hook
 				(add-hook mode (lambda () (display-line-numbers-mode 0))))))
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 25)))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . raindbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  ;(setq which-key-idle-delay 0.3))
+  )
 
 (use-package ivy
 	:diminish 
@@ -46,6 +71,10 @@
 	:config
 	(ivy-mode 1))
 
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
 (use-package evil
   :init
   (setq evil-want-integration t)
@@ -54,4 +83,26 @@
   (setq evil-want-C-i-jump nil)
   :config
   (evil-mode 1)
-  )
+)
+
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	("C-x b" . counsel-ibuffer)
+	("C-x C-f" . counsel-find-file))
+  :config
+  (setq ivy-initial-input-alist nil))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ivy-rich which-key use-package rainbow-delimiters evil doom-modeline counsel command-log-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
